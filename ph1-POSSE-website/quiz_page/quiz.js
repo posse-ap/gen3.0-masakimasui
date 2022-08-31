b73a462d2aea5fff67cec9b54c441aa1072a9d37
masakimasui <m.masui2003@gmail.com>
2022-08-31 14:14:56 +0900

    選択肢をクリックしたら下に、解説を表示する仕組みができました。

--- ph1-POSSE-website/quiz_page/quiz.js
+++ ph1-POSSE-website/quiz_page/quiz.js
@@ -1,8 +1,42 @@
-const answerChoices = document.querySelectorAll('.answer_choice');
-answerChoices.forEach( (answerChoice) => {
-  answerChoice.addEventListener('click', () => {
-    answerChoice.classList.add('is_clicked')
-  })
-})
-// 一回クリックしたら他のやつはクリックできないようにする
-let correct_html = ''
+{
+  const answers = [
+    "約79万人",
+    "X-TECH",
+    "Internet of Things",
+    "Society5.0",
+    "Web3.0",
+    "約5倍",
+  ];
+
+  // クイズごとに1回だけクリックを可能にするプログラム
+  for (let quizNumber = 1; quizNumber <= 6; quizNumber++) {
+    answerChoices = document.querySelectorAll(".quiz_number_" + quizNumber);
+    let clickNumber = 0;
+
+    answerChoices.forEach((answerChoice) => {
+      answerChoice.addEventListener("click", () => {
+        if (clickNumber === 0) {
+          clickNumber++;
+          answerChoice.classList.add("is_clicked");
+
+          let correctExplanation = `<div class="correct_explanation">
+          <div class="correct_in_text">正解！</div>
+          <div class="answer_explanation_sentence">
+            <div class="answer_explanation_A">A</div>
+            <div class="answer_explanation_content">${answers[quizNumber - 1]}</div>
+          </div>
+      </div>`;
+      console.log(answerChoice)
+          //クリックしたら上記のHTMLを表示する
+          //⬇️insertAdjacentHTMLでどこの下に置きたいかを表示するために取得した要素
+          const answerText = document.querySelectorAll(".answer_choices");
+          // クリックしたらHTMLを表示する
+          answerText[quizNumber - 1].insertAdjacentHTML(
+            "afterend",
+            correctExplanation
+          );
+        }
+      });
+    });
+  }
+}
